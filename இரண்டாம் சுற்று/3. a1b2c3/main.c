8a7316c1ae148e29f905558927dfe767f26bf086
Solaimani <89968516+Solaman10@users.noreply.github.com>
2022-01-06 18:13:16 +0530

    Update main.c

--- இரண்டாம் சுற்று/3. a1b2c3/main.c
+++ இரண்டாம் சுற்று/3. a1b2c3/main.c
@@ -2,26 +2,26 @@
 int main()
 {
     char s[50];
-    char alpha=0;
     int i,j,num=0;
-    printf("Enter the string with numbers you want: ");
+    char alpha=0;
+    printf("Enter the letters with numbers you want: ");
     scanf("%s",s);
     
-    for(i=0;s[i]!='\0';i++)
-    { 
-        if((s[i]>=97 && s[i]<=122) || (s[i]>=65 && s[i]<=90))
+    for(i=0;s[i];i++) // or for(i=0;s[i]!='\0';i++)
+    {
+        if((s[i]>=65 && s[i]<=90) || (s[i]>=97 && s[i]<=122))
         {
             if(alpha!=0)
             {
                 for(j=0;j<num;j++)
                 printf("%c",alpha);
-                num = 0;
+                num=0;
             }
-            alpha = s[i];
+            alpha=s[i];
         }
-        else if(s[i]>=48 && s[i<=57])
+        else if(s[i]>=48 && s[i]<=57)
         {
-            num =num*10+s[i]-48;
+            num = num*10+s[i]-48;
         }
     }
     for(j=0;j<num;j++)
