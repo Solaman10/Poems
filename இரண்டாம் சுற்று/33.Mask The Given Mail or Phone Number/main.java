2dbc5d1adb760a56bf3c576371937ae6a48e6c48
Solaimani <89968516+Solaman10@users.noreply.github.com>
2022-07-04 19:26:07 +0530

    Create main.java

---
+++ இரண்டாம் சுற்று/33.Mask The Given Mail or Phone Number/main.java
@@ -0,0 +1,40 @@
+/*
+i/p:
+Zoho@Zoho.com
+
+o/p:
+z*****o@zoho.com
+------------------
+i/p:
++91-1234-(567)-890
+
+o/p:
++91-*****7890
+*/
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class maskMailPhone {
+
+    public static String maskPII(String S) {
+        String countryCode = S.substring(0,4);
+        int at = S.indexOf("@");
+        if (at > 0)
+        {
+            S = S.toLowerCase();
+            return (S.charAt(0) + "*****" + S.substring(at - 1)).toLowerCase();
+        }
+        S = S.replaceAll("[^0-9]", "");
+        return countryCode + "*****" + S.substring(S.length() - 4);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String str;
+        str = sc.nextLine();
+
+        System.out.println(maskPII(str));
+    }
+}
