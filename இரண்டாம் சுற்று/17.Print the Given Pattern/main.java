a801ef1bd9d71ca04d22b8006b284616bbdba1d6
Solaimani <89968516+Solaman10@users.noreply.github.com>
2022-07-08 13:07:55 +0530

    Create main.java

---
+++ இரண்டாம் சுற்று/17.Print the Given Pattern/main.java
@@ -0,0 +1,45 @@
+/*
+i/p:
+5
+
+o/p:
+              1
+           2  6
+        3  7 10
+     4  8 11 13
+  5  9 12 14 15
+*/
+
+import java.util.Scanner;
+public class doAndErase
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int size,i,j,k,prntVal,addVal;
+        size = sc.nextInt();
+
+        for(i=1;i<=size;i++)
+        {
+            addVal=size-1;
+            for(j=i;j<=size;j++)
+            {
+                if(j==size)
+                {
+                    System.out.printf("%6d",i);
+                    prntVal=i;
+
+                    for(k=size-i+2;k<=size;k++)
+                    {
+                        prntVal=prntVal+addVal;
+                        System.out.printf("%6d",prntVal);
+                        addVal--;
+                    }
+                    System.out.println();
+                }
+                else
+                    System.out.print("      ");
+            }
+        }
+    }
+}
