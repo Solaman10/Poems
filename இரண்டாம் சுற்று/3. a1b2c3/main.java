0f1a543cee82993939d528f16c3f1f0e4c9a5605
Solaimani <89968516+Solaman10@users.noreply.github.com>
2022-07-28 11:48:04 +0530

    Update main.java

--- இரண்டாம் சுற்று/3. a1b2c3/main.java
+++ இரண்டாம் சுற்று/3. a1b2c3/main.java
@@ -6,35 +6,39 @@ o/p:
 abbccc
 */
 
-import java.util.Scanner;
+import java.util.*;
 
 public class ExpandString {
+
     public static void main(String[] args) {
-        String expr;
-        Scanner sc=new Scanner(System.in);
-        expr = sc.nextLine();
-        int ind=0,ind2, tempVal=0;
-        char tempChar = '\0';
 
-        while(ind<expr.length())
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        int ind=0,ind2,num=0;
+        char ch = '\0';
+
+        while(ind<str.length())
         {
-            if((expr.charAt(ind)>=65 && expr.charAt(ind)<=90) || (expr.charAt(ind)>=97 && expr.charAt(ind)<=122))
+            if(str.charAt(ind) >= 65 && str.charAt(ind) <= 90 || str.charAt(ind) >= 97 && str.charAt(ind) <= 122)
             {
-                tempChar = expr.charAt(ind);
-                ++ind;
+                if(ch!='\0')
+                {
+                    for(ind2=0;ind2<num;ind2++)
+                        System.out.print(ch);
+                        num=0;
+                }
+                ch = str.charAt(ind);
             }
-            else if (expr.charAt(ind) >= 48 && expr.charAt(ind) <= 57)
+            else if(str.charAt(ind) >= 48 && str.charAt(ind) <= 57)
             {
-                    tempVal = expr.charAt(ind) - 48;
-
-                    for (ind2 = 1; ind2 <= tempVal; ind2++)
-                    {
-                        System.out.print(tempChar);
-                    }
-                    tempVal = 0;
-                    ++ind;
-
+                num = num*10+str.charAt(ind) - 48;
             }
+            ++ind;
+        }
+        
+        for(ind2=0;ind2<num;ind2++)
+        {
+            System.out.print(ch);
         }
     }
 }
