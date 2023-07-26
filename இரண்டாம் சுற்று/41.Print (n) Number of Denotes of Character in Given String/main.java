560bf57f9b30cba95bc0e8424edb1df48b3c867f
Solaimani <89968516+Solaman10@users.noreply.github.com>
2023-07-26 19:15:12 +0530

    Update main.java

--- இரண்டாம் சுற்று/41.Print (n) Number of Denotes of Character in Given String/main.java
+++ இரண்டாம் சுற்று/41.Print (n) Number of Denotes of Character in Given String/main.java
@@ -25,64 +25,47 @@ public class doAndErase{
         String alpha = "";
         ArrayList<String> stack = new ArrayList<>();
 
-        for (int i = 0; i < s.length(); i++) 
-        {
+        for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
             
             if (ch == '(') 
-            {
                 stack.add(""+ch);
-            } 
-            else if (ch >= 'a' && ch <= 'z') 
-            {
+            else if (ch >= 'a' && ch <= 'z') {
                 alpha += ch;
+                
                 if (!stack.isEmpty() && !stack.get(stack.size()-1).equals("(")) 
-                {
                     alpha = stack.remove(stack.size()-1) + alpha;
-                }
+                
                 stack.add(alpha);
                 alpha = "";
             } 
-            else if (ch == ')') 
-            {
+            else if (ch == ')') {
                 temp = stack.remove(stack.size()-1);
-
+                
                 for(; !stack.get(stack.size()-1).equals("(");) 
-                {
                     temp = stack.remove(stack.size()-1) + temp;
-                }
 
                 stack.remove(stack.size()-1);
                 stack.add(temp);
             } 
-            else if (ch == '[') 
-            {
+            else if (ch == '[') {
                 i++;
                 temp = stack.remove(stack.size()-1);
                 String t = temp;
 
                 if(s.charAt(i)=='0') 
-                {
                     temp = "";
-                }
 
                 for(int j = 1; j < s.charAt(i)-48; j++) 
-                {
-                    temp += t;
-                }
-                
+                    temp += t;                
                 i++;
                 
-                if (s.charAt(i) == ']') 
-                {
+                if(s.charAt(i) == ']') 
                     stack.add(temp);
-                }
             }
         }
 
         for(int i=0;i<stack.size();i++)
-        {
             System.out.print(stack.get(i));
-        }
     }
 }
