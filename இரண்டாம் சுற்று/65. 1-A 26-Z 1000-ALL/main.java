84b65ec50dcc28956be287dd716dd0abfdb77c65
Solaimani <89968516+Solaman10@users.noreply.github.com>
2023-08-25 23:39:37 +0530

    Update main.java

--- இரண்டாம் சுற்று/65. 1-A 26-Z 1000-ALL/main.java
+++ இரண்டாம் சுற்று/65. 1-A 26-Z 1000-ALL/main.java
@@ -19,23 +19,20 @@ import java.util.*;
 
 public class Main
 {
-    public static String convertToTitle(int columnNum){
-        String ans = "";
-        while(columnNum>0){
-            columnNum--;
-            ans = (char) ('A'+(columnNum%26)) + ans;
-            columnNum /= 26;
-        }
-        
-        return ans;
-    }
+	public static String convertToTitle(int columnNum){
+		String ans = "";
+		while(columnNum>0){
+		    columnNum--;
+		    ans = (char) ('A'+(columnNum%26)) + ans;
+		    columnNum /= 26;
+		}
+		
+		return ans;
+	}
     
 	public static void main(String[] args) {
-	    
-	    Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        
-        System.out.print(convertToTitle(num));
-
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		System.out.print(convertToTitle(num));
 	}
 }
